5aa74750a8a52f04e2904d3b3a9fe1fc7b77e323
doyun.kim <gbyl2024@gmail.com>
2024-07-05 11:03:37 +0900

    add DeviceDriver function with TDD

--- Project24/DeviceDriver.cpp
+++ Project24/DeviceDriver.cpp
@@ -1,16 +1,44 @@
 #include "DeviceDriver.h"
+#include <stdexcept>
+#include <iostream>
+
+using namespace std;
+
+class WriteFailException :public std::exception {
+};
+
+class ReadFailException :public std::exception {
+};
+
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int readValue = (int)(m_hardware->read(address));
+    readConditionCheck(readValue, address);
+    return readValue;
+}
+
+void DeviceDriver::readConditionCheck(int readValue, long address)
+{
+    for (int i = 0; i < 4; i++) {
+        if (readValue != (int)(m_hardware->read(address)))
+            throw ReadFailException();
+    }
 }
 
 void DeviceDriver::write(long address, int data)
 {
-    // TODO: implement this method
+    writeConditionCheck(address);
     m_hardware->write(address, (unsigned char)data);
+}
+
+void DeviceDriver::writeConditionCheck(long address)
+{
+    int readValue = (int)(m_hardware->read(address));
+    if (readValue != BLANK_VALUE) {
+        throw WriteFailException();
+    }
 }
--- Project24/DeviceDriver.h
+++ Project24/DeviceDriver.h
@@ -10,4 +10,9 @@ public:
 
 protected:
     FlashMemoryDevice* m_hardware;
+
+private:
+    void readConditionCheck(int readValue, long address);
+    void writeConditionCheck(long address);
+    const int BLANK_VALUE = 0xFF;
 };
